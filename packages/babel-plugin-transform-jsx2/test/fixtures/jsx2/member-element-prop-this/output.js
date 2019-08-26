f8f4da4ca4b518fd459ab7e84e77b880850e60dc
Justin Ridgewell <justin@ridgewell.name>
2019-08-26 03:08:58 -0400

    MORE TESTS!!!

---
+++ packages/babel-plugin-transform-jsx2/test/fixtures/jsx2/member-element-prop-this/output.js
@@ -0,0 +1,15 @@
+var _ref = {
+  type: jsx2.expression,
+  key: null,
+  ref: null,
+  props: null,
+  constructor: void 0
+};
+
+function test() {
+  return {
+    template: _ref,
+    quasis: [xml.this],
+    constructor: void 0
+  };
+}
---
+++ packages/babel-plugin-transform-jsx2/test/fixtures/jsx2/namespace-element/input.js
@@ -0,0 +1,3 @@
+function test() {
+  return <xml:foo></xml:foo>;
+}
