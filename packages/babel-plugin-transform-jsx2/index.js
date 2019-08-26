54cac9f238125866a4eecec86888a0865b01ae6d
Justin Ridgewell <justin@ridgewell.name>
2019-08-25 22:34:25 -0400

    Support JSXSpreadAttribute

--- packages/babel-plugin-transform-jsx2/index.js
+++ packages/babel-plugin-transform-jsx2/index.js
@@ -11,19 +11,19 @@ module.exports = function({ types: t, template }) {
 
   return { visitor };
 
-  function buildElement(path, expressions) {
-    const root = expressions === undefined;
-    if (!expressions) expressions = [];
+  function buildElement(path, quasis) {
+    const root = quasis === undefined;
+    if (!quasis) quasis = [];
 
     const frag = path.isJSXFragment();
     const opening = path.get('openingElement');
     const type = frag
       ? template.expression.ast`jsx2.Fragment`
-      : elementType(convertJSXName(opening.get('name')), expressions);
+      : elementType(convertJSXName(opening.get('name')), quasis);
     const { props, key, ref } = buildProps(
       frag ? [] : opening.get('attributes'),
       path.get('children'),
-      expressions
+      quasis
     );
 
     const objProps = [
@@ -39,42 +39,45 @@ module.exports = function({ types: t, template }) {
 
     return t.objectExpression([
       t.objectProperty(t.identifier('template'), obj),
-      t.objectProperty(t.identifier('expressions'), t.arrayExpression(expressions)),
+      t.objectProperty(t.identifier('quasis'), t.arrayExpression(quasis)),
     ]);
   }
 
-  function buildProps(attributePaths, childPaths, expressions) {
+  function buildProps(attributePaths, childPaths, quasis) {
     const children = [];
     let key = t.nullLiteral();
     let ref = t.nullLiteral();
     let childrenProp;
 
+    let objs = [];
     let objProps = [];
     for (let i = 0; i < attributePaths.length; i++) {
       const attribute = attributePaths[i];
 
       if (attribute.isJSXSpreadAttribute()) {
-        throw attribute.buildCodeFrameError('unsupported');
+        objProps = pushProps(objProps, objs);
+        quasis.push(attribute.node.argument);
+        objs.push(template.expression.ast`jsx2.expression`);
+        continue;
       }
 
       const name = attribute.get('name');
       const value = attribute.get('value');
 
       if (name.isIdentifier({ name: 'key' })) {
-        key = extractAttributeValue(value, expressions);
+        key = extractAttributeValue(value, quasis);
         continue;
       } else if (name.isIdentifier({ name: 'ref' })) {
-        ref = extractAttributeValue(value, expressions);
+        ref = extractAttributeValue(value, quasis);
         continue;
       } else if (name.isIdentifier({ name: 'children' })) {
-        childrenProp = extractAttributeValue(value, expressions);
+        childrenProp = extractAttributeValue(value, quasis);
         continue;
       }
 
-      objProps.push(
-        t.objectProperty(convertJSXName(name), extractAttributeValue(value, expressions))
-      );
+      objProps.push(t.objectProperty(convertJSXName(name), extractAttributeValue(value, quasis)));
     }
+    pushProps(objProps, objs);
 
     for (let i = 0; i < childPaths.length; i++) {
       const child = childPaths[i];
@@ -83,9 +86,13 @@ module.exports = function({ types: t, template }) {
         continue;
       }
 
-      if (child.isJSXSpreadChild()) throw child.buildCodeFrameError('unsupported');
+      if (child.isJSXSpreadChild()) {
+        quasis.push(t.arrayExpression([t.spreadElement(child.node.expression)]));
+        children.push(template.expression.ast`jsx2.expression`);
+        continue;
+      }
 
-      children.push(extractValue(child, expressions));
+      children.push(extractValue(child, quasis));
     }
 
     if (children.length || childrenProp) {
@@ -93,34 +100,45 @@ module.exports = function({ types: t, template }) {
       objProps.push(t.objectProperty(t.identifier('children'), c));
     }
 
-    const props = objProps.length ? t.objectExpression(objProps) : t.nullLiteral();
+    const props = objs.length
+      ? objs.length === 1 && t.isObjectExpression(objs[0])
+        ? objs[0]
+        : t.arrayExpression(objs)
+      : t.nullLiteral();
 
     return { key, ref, props };
   }
 
-  function extractAttributeValue(value, expressions) {
-    if (!value.node) return t.booleanLiteral(true);
-    return extractValue(value, expressions);
+  function pushProps(objProps, objs) {
+    if (!objProps.length) return objProps;
+
+    objs.push(t.objectExpression(objProps));
+    return [];
   }
 
-  function extractValue(value, expressions) {
+  function extractAttributeValue(value, quasis) {
+    if (!value.node) return t.booleanLiteral(true);
+    return extractValue(value, quasis);
+  }
+
+  function extractValue(value, quasis) {
     if (value.isJSXElement() || value.isJSXFragment()) {
-      return buildElement(value, expressions);
+      return buildElement(value, quasis);
     }
     if (!value.isJSXExpressionContainer()) return value.node;
 
     const expression = value.get('expression');
 
     if (expression.isJSXElement() || expression.isJSXFragment()) {
-      return buildElement(expression, expressions);
+      return buildElement(expression, quasis);
     }
-    if (expression.isLiteral()) return expression.node;
+    if (expression.isPure()) return expression.node;
 
-    expressions.push(expression.node);
+    quasis.push(expression.node);
     return template.expression.ast`jsx2.expression`;
   }
 
-  function elementType(node, expressions) {
+  function elementType(node, quasis) {
     if (t.isStringLiteral(node)) return node;
 
     if (t.isIdentifier(node)) {
@@ -128,7 +146,7 @@ module.exports = function({ types: t, template }) {
       if (t.react.isCompatTag(name)) return t.stringLiteral(name);
     }
 
-    expressions.push(node);
+    quasis.push(node);
     return template.expression.ast`jsx2.expression`;
   }
 
