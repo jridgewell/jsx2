9edac30313f735de896cd687b173bf62077365fc
Justin Ridgewell <justin@ridgewell.name>
2019-11-15 01:48:08 -0500

    Always set indices

--- packages/jsx2/src/fiber/mark.ts
+++ packages/jsx2/src/fiber/mark.ts
@@ -3,7 +3,7 @@ type Fiber = import('.').Fiber;
 export function mark(current: Fiber, parent: Fiber, previous: null | Fiber): void {
   current.parent = parent;
   if (previous) {
-    debug: current.index = previous.index + 1;
+    current.index = previous.index + 1;
     previous.next = current;
   } else {
     parent.child = current;
