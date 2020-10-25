d0646820c0d3f3820f152d4e4e7167a0599b47eb
Justin Ridgewell <justin@ridgewell.name>
2020-10-24 22:07:06 -0400

    Upgrade deps

--- packages/jsx2/rollup.config.ts
+++ packages/jsx2/rollup.config.ts
@@ -1,38 +1,23 @@
-import commonjs from 'rollup-plugin-commonjs';
-import resolve from 'rollup-plugin-node-resolve';
-import sourceMaps from 'rollup-plugin-sourcemaps';
-import typescript from 'rollup-plugin-typescript';
+import typescript from '@rollup/plugin-typescript';
 import strip from '@rollup/plugin-strip';
 
-const pkg = require('./package.json');
+function configure(esm) {
+  return {
+    input: 'src/jsx2.ts',
+    output:  esm
+        ? { format: 'es', dir: 'dist', entryFileNames: '[name].mjs', sourcemap: true }
+        : { format: 'umd', name: 'dedent', dir: 'dist', entryFileNames: '[name].umd.js', sourcemap: true },
+    plugins: [
+      // Compile TypeScript files
+      typescript(),
 
-export default {
-  external: [],
-  input: `src/jsx2.ts`,
-  output: [
-    { file: pkg.main, name: 'jsx2', format: 'umd', sourcemap: true },
-    { file: pkg.module, format: 'es', sourcemap: true },
-  ],
-  plugins: [
-    // Compile TypeScript files
-    typescript(),
+      // Eliminate debug blocks.
+      strip({ include: '**/*.ts', functions: [], labels: ['debug'] }),
+    ],
+    watch: {
+      include: 'src/**',
+    },
+  };
+}
 
-    // Eliminate debug blocks.
-    strip({ include: '**/*.ts', functions: [], labels: ['debug'] }),
-
-    // Allow bundling cjs modules (unlike webpack, rollup doesn't understand cjs)
-    commonjs(),
-
-    // Allow node_modules resolution, so you can use 'external' to control
-    // which external modules to include in the bundle
-    // https://github.com/rollup/rollup-plugin-node-resolve#usage
-    resolve(),
-
-    // Resolve source maps to the original source
-    sourceMaps(),
-  ],
-  // Indicate here external modules you don't wanna include in your bundle (i.e.: 'lodash')
-  watch: {
-    include: 'src/**',
-  },
-};
+export default [configure(false), configure(true)];
