f03358bbb4c121cfb48d5ed4a1c54161b4b2d62f
Justin Ridgewell <justin@ridgewell.name>
2020-10-24 23:02:14 -0400

    Include prettier in eslint

--- packages/jsx2/.eslintrc.js
+++ packages/jsx2/.eslintrc.js
@@ -2,7 +2,12 @@ module.exports = {
   root: true,
   parser: '@typescript-eslint/parser',
   plugins: ['@typescript-eslint'],
-  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+    'prettier/@typescript-eslint',
+  ],
   rules: {
     '@typescript-eslint/no-explicit-any': 'off',
     '@typescript-eslint/no-non-null-assertion': 'off',
