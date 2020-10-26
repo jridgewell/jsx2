7ccdcb735b0f51bb97bfed788182df864eeba27d
Justin Ridgewell <justin@ridgewell.name>
2020-10-26 05:07:05 -0400

    Rename templateResult to templateBlock

---
+++ packages/jsx2/src/template-result.ts
@@ -0,0 +1,50 @@
+export type Primitive = string | boolean | null;
+export type Marker = number;
+
+export type PropValue = Primitive | Marker | StaticNode;
+export type Props = {
+  readonly children?: PropValue | readonly PropValue[];
+} & Readonly<Record<string, PropValue>>;
+
+export interface StaticNode {
+  readonly type: string | Marker;
+  readonly key?: string | null | Marker;
+  readonly ref?: null | Marker;
+  readonly props?: null | Marker | Props | readonly (Marker | Props)[];
+}
+
+export interface TemplateBlock {
+  readonly tree: StaticNode;
+  readonly expressions: unknown[];
+  readonly constructor: undefined;
+}
+
+const parsed = new WeakMap<TemplateStringsArray, StaticNode>();
+
+function parse(strings: TemplateStringsArray): StaticNode {
+  let s = strings[0];
+  for (let i = 1; i < strings.length; i++) {
+    s = s + (i - 1) + strings[i];
+  }
+  return JSON.parse(s) as StaticNode;
+}
+
+function getTree(strings: TemplateStringsArray): StaticNode {
+  let tree = parsed.get(strings);
+  if (tree) return tree;
+
+  tree = parse(strings);
+  parsed.set(strings, tree);
+  return tree;
+}
+
+export function templateBlock(
+  strings: TemplateStringsArray,
+  ...expressions: unknown[]
+): TemplateBlock {
+  return {
+    tree: getTree(strings),
+    expressions,
+    constructor: void 0,
+  };
+}
