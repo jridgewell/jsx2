fa38ff5e8b3b8c1cf2e1266cbca4a15f7744775a
Justin Ridgewell <justin@ridgewell.name>
2020-10-27 04:17:34 -0400

    Test getContainer

--- packages/jsx2/src/fiber/get-container.ts
+++ packages/jsx2/src/fiber/get-container.ts
@@ -2,7 +2,7 @@ import type { Fiber } from '.';
 export function getContainer(fiber: Fiber): Fiber['dom'] {
   let current: null | Fiber = fiber;
   do {
-    const { dom } = fiber;
+    const { dom } = current;
     if (dom) return dom;
     current = current.parent;
   } while (current);
