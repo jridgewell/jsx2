dab9433ff62d26a70943390a4d84346f6bd745a7
Justin Ridgewell <justin@ridgewell.name>
2020-10-26 21:06:11 -0400

    Fixup triangle demo

--- packages/demo/src/triangle.js
+++ packages/demo/src/triangle.js
@@ -1,6 +1,6 @@
-import { render, createElement, useState, useEffect, useCallback } from '/jsx2/jsx2';
+import { render, createElement, useState, useEffect, useCallback, useMemo } from '/jsx2/jsx2';
 
-var dotStyle = {
+const dotStyle = {
   position: 'absolute',
   background: '#61dafb',
   font: 'normal 15px sans-serif',
@@ -8,7 +8,7 @@ var dotStyle = {
   cursor: 'pointer',
 };
 
-var containerStyle = {
+const containerStyle = {
   position: 'absolute',
   transformOrigin: '0 0',
   left: '50%',
@@ -18,26 +18,26 @@ var containerStyle = {
   background: '#eee',
 };
 
-var targetSize = 25;
+const targetSize = 25;
 
-function Dot(props) {
+function Dot({ x, y, size, text }) {
   const [hover, setHover] = useState(false);
   const enter = useCallback(() => setHover(true));
   const leave = useCallback(() => setHover(false));
-  var s = props.size * 1.3;
-  var style = {
+  const s = size * 1.3;
+  const style = {
     ...dotStyle,
     width: s + 'px',
     height: s + 'px',
-    left: props.x + 'px',
-    top: props.y + 'px',
+    left: x + 'px',
+    top: y + 'px',
     borderRadius: s / 2 + 'px',
     lineHeight: s + 'px',
     background: hover ? '#ff0' : dotStyle.background,
   };
   return (
     <div style={style} onMouseEnter={enter} onMouseLeave={leave}>
-      {hover ? '*' + props.text + '*' : props.text}
+      {hover ? '*' + text + '*' : text}
     </div>
   );
 }
@@ -45,40 +45,35 @@ function Dot(props) {
 function SierpinskiTriangle({ x, y, s, children }) {
   if (s <= targetSize) {
     return <Dot x={x - targetSize / 2} y={y - targetSize / 2} size={targetSize} text={children} />;
-    return r;
   }
-  var newSize = s / 2;
-  var slowDown = true;
+  const slowDown = true;
   if (slowDown) {
-    var e = performance.now() + 0.8;
+    const e = performance.now() + 0.8;
     while (performance.now() < e) {
       // Artificially long execution time.
     }
   }
 
   s /= 2;
-
-  return (
-    <div>
-      <SierpinskiTriangle x={x} y={y - s / 2} s={s}>
-        {children}
-      </SierpinskiTriangle>
-      <SierpinskiTriangle x={x - s} y={y + s / 2} s={s}>
-        {children}
-      </SierpinskiTriangle>
-      <SierpinskiTriangle x={x + s} y={y + s / 2} s={s}>
-        {children}
-      </SierpinskiTriangle>
-    </div>
+  return useMemo(
+    () => (
+      <div>
+        <SierpinskiTriangle x={x} y={y - s / 2} s={s}>
+          {children}
+        </SierpinskiTriangle>
+        <SierpinskiTriangle x={x - s} y={y + s / 2} s={s}>
+          {children}
+        </SierpinskiTriangle>
+        <SierpinskiTriangle x={x + s} y={y + s / 2} s={s}>
+          {children}
+        </SierpinskiTriangle>
+      </div>
+    ),
+    [x, y, s, children],
   );
 }
-SierpinskiTriangle.shouldComponentUpdate = function (oldProps, newProps) {
-  var o = oldProps;
-  var n = newProps;
-  return !(o.x === n.x && o.y === n.y && o.s === n.s && o.children === n.children);
-};
 
-function App(props) {
+function App({ elapsed }) {
   const [seconds, setSeconds] = useState(0);
   useEffect(() => {
     const interval = setInterval(() => {
@@ -87,7 +82,6 @@ function App(props) {
     return () => clearInterval(interval);
   }, []);
 
-  const elapsed = props.elapsed;
   const t = (elapsed / 1000) % 10;
   const scale = 1 + (t > 5 ? 10 - t : t) / 10;
   const transform = 'scaleX(' + scale / 2.1 + ') scaleY(0.7) translateZ(0.1px)';
@@ -102,8 +96,8 @@ function App(props) {
   );
 }
 
-var start = new Date().getTime();
-var root = document.getElementById('root');
+const start = new Date().getTime();
+const root = document.getElementById('root');
 function update() {
   render(<App elapsed={new Date().getTime() - start} />, root);
   requestAnimationFrame(update);
