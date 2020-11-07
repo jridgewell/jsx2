60952d3be172987d508c8ca3defff544252ec817
Justin Ridgewell <justin@ridgewell.name>
2020-11-07 18:27:59 -0500

    Explicitly set all options in tests

--- packages/babel-plugin-transform-jsx2/test/fixtures/jsx2-pretty-json/child-expression-bigint/output.js
+++ packages/babel-plugin-transform-jsx2/test/fixtures/jsx2-pretty-json/child-expression-bigint/output.js
@@ -1,5 +1,20 @@
 var _templateBlock = require("jsx2").templateBlock;
 
 function test() {
-  return _templateBlock`{"type":"div","key":"","ref":null,"props":{"children":${1n}}}`;
+  return _templateBlock(_template(), [1n]);
+}
+
+function _template() {
+  const tree = JSON.parse(`{
+  "type": "div",
+  "key": "",
+  "ref": null,
+  "props": {
+    "children": 0
+  }
+}`);
+
+  _template = () => tree;
+
+  return tree;
 }
